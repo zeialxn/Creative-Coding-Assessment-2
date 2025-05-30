14c4857e4f7456d4c5c2f0dde565a635d863abf9
Zhealene Lucero <eihracalibara@gmail.com>
2025-05-30 15:31:45 +0400

    Basic Car

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB);
+  background(34, 31, 96);
+  stroke(4, 59, 28);
+  strokeWeight(5);
+  fill(4, 59, 28);
+  ellipse(100,300,90);
+  ellipse(300,300,90);
+  fill(13, 95, 73);
+  ellipse(200,170,250);
+  fill(13, 95, 73);
+  rect(50,160,300,140);
+  fill(43, 23, 99);
+  rect(70,220,50);
+  rect(280,220,50);
+  line(140,260,250,260);
+  line(140,250,250,250);
+  line(140,240,250,240);
+  line(140,230,250,230);
+}
