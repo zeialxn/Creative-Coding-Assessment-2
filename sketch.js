2249865f1048411acb7d8e2f24492074d42aa9a2
Zhealene Lucero <eihracalibara@gmail.com>
2025-05-30 15:34:16 +0400

    Make Your Own Alien

--- sketch.js
+++ sketch.js
@@ -1,63 +1,51 @@
-let ball;
-let ballSpeed = -1;
-
 function setup() {
   createCanvas(400, 400);
-  noStroke();
-  ball = height;
+  colorMode(HSL);
 }
 
 function draw() {
-  background(0, 150, 0);
-
-  let x = width / 2;
-  let y = map(ball, height, 70, 130, 30);  
-
-  drawSky();
-  drawGoal();
-
-  fill(230, 230, 230);
-  ellipse(x, ball, y, y);
-
-  if (ball > 70) {
-    ball += ballSpeed;
-  }
-}
-
-function drawSky() {
-  fill(200, 250, 255);
-  rect(0, 0, 400, 90);
-}
-
-function drawGoal() {
-  push();
-  fill(255, 255, 255);
-  rect(100, 40, 200, 50);
-  pop();
-
-  push();
-  fill(0, 150, 0);
-  stroke(255);
-  strokeWeight(10);
-  ellipse(200, 140, 160, 100);
-  pop();
-
-  push();
-  fill(0, 150, 0);
-  stroke(255);
-  strokeWeight(10);
+  background(65, 76, 82);
+  
+  //shadow
+  fill( color('#C4CF79') );
+  ellipse(200,350,130,40)
+  
+  fill( color('#D2E071') );
+  // head
+  ellipse(200, 150, 180, 150)
+  noStroke();
+  
+  // arms
+  ellipse(140,240,50,70)
+  ellipse(260,240,50,70)
+  
+  // body
   beginShape();
-  vertex(102, 95);
-  vertex(298, 95);
-  vertex(350, 150);
-  vertex(50, 150);
+  vertex(150, 210);
+  vertex(250, 210);
+  bezierVertex(280, 260, 280, 320, 200, 340);
+  bezierVertex(120, 320, 120, 260, 150, 210);
   endShape(CLOSE);
-  pop();
-
+  endShape();
+  
+  // ears
+  rect(150,60,20,40);
+  rect(230,60,20,40);
+  ellipse(160,65,30,20);
+  ellipse(240,65,30,20);
+  
+  // eyes
   push();
-  fill(0, 150, 0);
-  stroke(255);
-  strokeWeight(15);
-  ellipse(200, 400, 300, 150);
+  fill( color('#FBFBEF') );
+  ellipse(160,160,70,60,);
+  ellipse(240,160,70,60,);
+  fill( color('#5A6518') );
+  ellipse(165,155,60,50,);
+  ellipse(235,155,60,50,);
   pop();
+  
+  // mouth
+  fill( color('#93A02D') );
+  arc(200, 195, 60, 40, 0, PI);
+  
 }
