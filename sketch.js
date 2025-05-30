87466e5159384890683c31c12d027810dad00d8d
Zhealene Lucero <eihracalibara@gmail.com>
2025-05-30 15:33:00 +0400

    Soccer Ball - Colorization

--- sketch.js
+++ sketch.js
@@ -1,21 +1,52 @@
 function setup() {
   createCanvas(400, 400);
-  colorMode(HSB);
-  background(34, 31, 96);
-  stroke(4, 59, 28);
-  strokeWeight(5);
-  fill(4, 59, 28);
-  ellipse(100,300,90);
-  ellipse(300,300,90);
-  fill(13, 95, 73);
-  ellipse(200,170,250);
-  fill(13, 95, 73);
-  rect(50,160,300,140);
-  fill(43, 23, 99);
-  rect(70,220,50);
-  rect(280,220,50);
-  line(140,260,250,260);
-  line(140,250,250,250);
-  line(140,240,250,240);
-  line(140,230,250,230);
+  noStroke();
+}
+
+function draw() {
+  background(0, 150, 0);
+
+  drawSky();
+  drawGoal();
+
+  fill(230, 230, 230);
+  ellipse(200,330,115,115);
+}
+
+function drawSky() {
+  fill(200, 250, 255);
+  rect(0, 0, 400, 90);
+}
+
+function drawGoal() {
+  push();
+  fill(255, 255, 255);
+  rect(100, 40, 200, 50);
+  pop();
+
+  push();
+  fill(0, 150, 0);
+  stroke(255);
+  strokeWeight(10);
+  ellipse(200, 140, 160, 100);
+  pop();
+
+  push();
+  fill(0, 150, 0);
+  stroke(255);
+  strokeWeight(10);
+  beginShape();
+  vertex(102, 95);
+  vertex(298, 95);
+  vertex(350, 150);
+  vertex(50, 150);
+  endShape(CLOSE);
+  pop();
+
+  push();
+  fill(0, 150, 0);
+  stroke(255);
+  strokeWeight(15);
+  ellipse(200, 400, 300, 150);
+  pop();
 }
